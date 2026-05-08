8f45453f4e9e0ba22198de77a9bd0a6a8d613ccc
murilogiacomini-rush <murilo.giacomini@edu.senai.br>
2026-05-08 09:54:11 -0300

    ex47

---
+++ ex47.js
@@ -0,0 +1,8 @@
+// Exercício 47: Valores Divisíveis por 4 Menores que 200
+
+console.log("Números divisíveis por 4 menores que 200:\n");
+
+// Processamento e Saída
+for (let numero = 4; numero < 200; numero += 4) {
+  console.log(numero);
+}
