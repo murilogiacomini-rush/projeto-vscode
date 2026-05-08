5e66a0daa6208b238fbd35e089c272094ef1859b
murilogiacomini-rush <murilo.giacomini@edu.senai.br>
2026-05-08 09:53:03 -0300

    ex46

---
+++ ex46.js
@@ -0,0 +1,11 @@
+// Exercício 46: Números Ímpares na Faixa de 0 a 200
+
+let numero = 1;
+
+// Processamento e Saída
+console.log("Números ímpares de 0 até 200:\n");
+
+while (numero <= 199) {
+  console.log(numero);
+  numero += 2;
+}
